764cedab6e717643d87b7e2aa3621b010cf30c92
Khun <khun.ly@bstorm.be>
2024-05-14 11:31:22 +0200

    icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {
   IonApp,
   IonContent,
-  IonHeader,
+  IonHeader, IonIcon,
   IonItem,
   IonMenu, IonNavLink,
   IonRouterOutlet,
@@ -10,13 +10,20 @@ import {
   IonToolbar
 } from '@ionic/angular/standalone';
 import {RouterLink} from "@angular/router";
+import {addIcons} from "ionicons";
+import {alarm, home} from "ionicons/icons";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonContent, IonTitle, IonItem, RouterLink, IonToolbar, IonNavLink],
+  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonContent, IonTitle, IonItem, RouterLink, IonToolbar, IonNavLink, IonIcon],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({
+      home,
+      alarm,
+    })
+  }
 }
