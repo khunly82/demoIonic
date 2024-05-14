cd311f0fcfc6b429a39b4e127764539701406e2e
Khun <khun.ly@bstorm.be>
2024-05-14 13:13:31 +0200

    correction chronometre

--- src/app/pages/chrono/chrono.page.ts
+++ src/app/pages/chrono/chrono.page.ts
@@ -1,20 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonButtons, IonContent, IonHeader, IonMenuButton, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle,
+  IonContent,
+  IonHeader, IonIcon,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import {addIcons} from "ionicons";
+import {codeWorking, play, refresh, stop} from "ionicons/icons";
+import {TimePipe} from "../../pipes/time.pipe";
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-chrono',
   templateUrl: './chrono.page.html',
   styleUrls: ['./chrono.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon, TimePipe]
 })
 export class ChronoPage implements OnInit {
 
-  constructor() { }
+  time: number = 0;
+  interval: any = null;
+  memo: number = 0;
+
+  constructor() {
+    addIcons({
+      play: play,
+      stop: stop,
+      refresh: refresh
+    })
+  }
 
   ngOnInit() {
+  }
+
+  start() {
+    if(this.interval) {
+      return;
+    }
+    const startTime = new Date().getTime();
+    this.interval = setInterval(() => {
+      this.time = new Date().getTime() - startTime + this.memo;
+    });
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    this.interval = null;
+    this.memo = this.time;
+  }
+
+  reset() {
+    this.time = 0;
+    this.stop();
   }
 
 }
